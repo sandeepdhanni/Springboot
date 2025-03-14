6dec27c01501cc7566689ceab68dd398dd80ba0a
sandeepdhanni <sandeepdhanni703@gmail.com>
2025-03-14 17:50:24 +0530

    reading html content in msg file

--- EmailReading/msg/src/main/java/com/example/msg/MsgReaderHtml.java
+++ EmailReading/msg/src/main/java/com/example/msg/MsgReaderHtml.java
@@ -1,16 +1,14 @@
 package com.example.msg;
 
+import com.aspose.email.system.exceptions.IOException;
 import org.apache.poi.hsmf.MAPIMessage;
 import org.apache.poi.hsmf.exceptions.ChunkNotFoundException;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 
-
-public class MsgReader {
-
-
+//this is msg file which contains the html content which can read
+public class MsgReaderHtml {
     public static void main(String[] args) {
         String filePath = "C:\\Users\\Sreenivas Bandaru\\Downloads\\a737b-snjcd.msg";
         System.out.println("Starting to read MSG file: " + filePath);
@@ -19,26 +17,24 @@ public class MsgReader {
 
     public static void readMsgFile(String filePath) {
         System.out.println("Attempting to open file: " + filePath);
-        try (MAPIMessage message = new MAPIMessage(filePath)) {
+        try (FileInputStream fis = new FileInputStream(new File(filePath))) {
             System.out.println("File opened successfully.");
+            MAPIMessage message = new MAPIMessage(fis);
             System.out.println("MAPIMessage object created.");
 
             String subject = message.getSubject();
             String from = message.getDisplayFrom();
             String to = message.getDisplayTo();
-            String body = message.getTextBody();
-            if (message.getHtmlBody().trim().isEmpty()) {
-                System.out.println("html body: "+message.getHtmlBody());
-            }
+            String htmlBody = message.getHtmlBody();
 
             System.out.println("Extracted Subject: " + subject);
             System.out.println("Extracted From: " + from);
             System.out.println("Extracted To: " + to);
-            System.out.println("Extracted Body: " + body);
+            System.out.println("Extracted HTML Body: " + htmlBody);
         } catch (ChunkNotFoundException e) {
             System.err.println("Error: Missing expected chunk in the MSG file.");
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (IOException | java.io.IOException e) {
             System.err.println("Error reading the MSG file.");
             e.printStackTrace();
         }
